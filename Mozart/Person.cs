72cfbd52e469417818dca5a2673e237e926c881d
Kenneth Folke Larsen <5504355@RTS-365.dk>
2020-02-04 16:39:15 +0100

    Committing so my arrays save Also added OOP structuce

---
+++ Mozart/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mozart
+{
+    abstract class Person
+    {
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+    }
+}
---
+++ Mozart/Piano.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Media;
+
+namespace Mozart
+{
+    class Piano
+    {
+        SoundPlayer soundPlayer = new SoundPlayer();
+        
+        //plays audio through media player
+        internal void PlayMusic(string song)
+        {
+                soundPlayer.SoundLocation = song;
+                soundPlayer.Load();
+                soundPlayer.PlaySync();
+         
+        }
+    }
+}
